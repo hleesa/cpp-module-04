ee58663b3bf6ff46f64c1bf5b25681ea7cfd3557
salee2 <hleesa>
2023-07-04 15:12:26 +0900

    add message

--- ex03/Cure.cpp
+++ ex03/Cure.cpp
@@ -2,15 +2,15 @@
 #include "Cure.hpp"
 
 Cure::Cure() : AMateria("cure") {
-	std::cout << "Default constructor called, AMateria type: " << type << std::endl;
+	std::cout << "Default constructor called, Cure type: " << type << std::endl;
 }
 
 Cure::Cure(const Cure& other) : AMateria(other.type) {
-	std::cout << "Copy constructor called, AMateria type: " << type << std::endl;
+	std::cout << "Copy constructor called, Cure type: " << type << std::endl;
 }
 
 Cure& Cure::operator=(const Cure& other) {
-	std::cout << "Copy assignment operator called, AMateria type: " << type << std::endl;
+	std::cout << "Copy assignment operator called, Cure type: " << type << std::endl;
 	if (this != &other) {
 		type = other.type;
 	}
@@ -18,7 +18,7 @@ Cure& Cure::operator=(const Cure& other) {
 }
 
 Cure:: ~Cure() {
-	std::cout << "Destructor called, AMateria type: " << type << std::endl;
+	std::cout << "Destructor called, Cure type: " << type << std::endl;
 }
 
 AMateria* Cure::clone() const {
